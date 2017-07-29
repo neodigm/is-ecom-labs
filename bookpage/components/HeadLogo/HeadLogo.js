1a6fc78a8a42c9b283058eca9960c196e3726656
neodigm <neo5ive@gmail.com>
2017-07-29 19:44:58 -0400

    content

--- bookpage/components/HeadLogo/HeadLogo.js
+++ bookpage/components/HeadLogo/HeadLogo.js
@@ -1,1 +1,6 @@
-console.log("component HeadLogo.js");
+console.log("component headlogo.js");
+console.log("Vue Version " +Vue.version );
+Vue.config.devtools = true;
+var c_headlogo = new Vue({
+  el: "js-headlogo"
+});
