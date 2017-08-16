4d9d457487bd1364fcf866287f3f45862b35861c
NicholasKrause <NicholasKrause@users.noreply.sudo>
2017-08-16 15:00:19 -0400

    content

--- bookpage/components/reveal/reveal.js
+++ bookpage/components/reveal/reveal.js
@@ -1,17 +1,34 @@
 console.log("component reveal.js");
-Vue.config.devtools = true;
-
-var QQQQc_footer2 = new Vue({
-  el: "#QQQQjs-footer2",
-  data: {
-    isOpenR__menu: false,
-    isOpenR__scrim: false,
-  },
-  methods: {
-    toggle: function( e ){
-      console.log("Reveal Clicked");
-      this.isOpenR__menu = !this.isOpenR__menu;
-      this.isOpenR__scrim = !this.isOpenR__scrim;
+var ltdc_reveal = {
+  aRevAct: 0, aRevX: 0, sRevId: "", eRev: 0, eRevScrim: 0,
+  "init" : function() {
+    aRevX = document.getElementsByClassName( "close-reveal-modal" );
+    for (var i = 0, ln = aRevX.length; i < ln; i++) {
+        aRevX[i].addEventListener("click", ltdc_reveal.close, false);
     }
+    aRevAct = document.querySelectorAll( "[data-reveal-id]" );
+    for (var i = 0, ln = aRevAct.length; i < ln; i++) {
+        aRevAct[i].addEventListener("click", ltdc_reveal.open, false);
+    }
+    eRevScrim = document.getElementById( "id-reveal__scrim" );
+  },
+  "open" : function( e ){
+    sRevId = e.target.getAttribute( "data-reveal-id" );
+    if( sRevId ){
+      eRevScrim.classList.add( "reveal__scrim" );
+      eRev = document.getElementById( sRevId );
+      eRev.classList.add( "reveal__box" );
+      eRev.setAttribute("style", "top: "+ (window.pageYOffset+84) +"px;");
+      eRev.setAttribute("aria-hidden", "false");
+      e.preventDefault();
+    }
+    return false;
+  },
+  "close" : function( e ){
+    eRevScrim.classList.remove( "reveal__scrim" );
+    eRev.classList.remove( "reveal__box" );
+    eRev.setAttribute("aria-hidden", "true");
+    e.preventDefault();
   }
-});
+};
+ltdc_reveal.init();
