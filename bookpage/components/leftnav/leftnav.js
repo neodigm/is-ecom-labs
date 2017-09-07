856cd26003b388830df1e4208f4ecc11f6e0e599
neodigm <neo5ive@gmail.com>
2017-09-07 10:30:25 -0500

    content

--- bookpage/components/leftnav/leftnav.js
+++ bookpage/components/leftnav/leftnav.js
@@ -36,10 +36,43 @@ Vue.component("ltdc-leftnav", {
   }
 })
 
-// boot up the v-leftnav__id
 var vleftnav__id = new Vue({
-  el: '#v-leftnav__id',
+  el: "#v-leftnav__id",
   data: {
-    ltdc_leftnav__hier: ltdc_leftnav__hier
+    ltdc_leftnav__hier: ltdc_leftnav
+  },
+  methods: {
+    parseJSONCat: function(){
+      var oPush={}, nLvl=0, sName="", sHref="", nL2Ct = -1;
+      for (var key in jsnCat) {
+          nLvl=1;
+          if(typeof( jsnCat[key].level2CategoryUrl ) !== "undefined"){ nLvl=2; }
+          if(typeof( jsnCat[key].level3CategoryUrl ) !== "undefined"){ nLvl=3; }
+          sName=jsnCat[key]["level"+nLvl+"DisplayName"];
+          sHref=jsnCat[key]["level"+nLvl+"CategoryUrl"];
+          switch( nLvl ){
+            case 1:
+              oPush={name: sName, href: sHref};
+              ltdc_leftnav.children.push( oPush );
+              nL2Ct = -1;
+            break;
+            case 2:
+              if(typeof( oPush.children )=="undefined"){ oPush.children=[]; }
+              oPush.children.push( {name: sName, href: sHref} );
+              nL2Ct++;
+            break;
+            case 3:
+              if(typeof( oPush.children[nL2Ct] )!="undefined"){
+                if(typeof( oPush.children[nL2Ct].children )=="undefined"){
+                    oPush.children[nL2Ct].children=[];
+                }
+                  oPush.children[nL2Ct].children.push( {name: sName, href: sHref} );
+              }
+          }
+      }
+    }
+  },
+  created: function(){
+    this.parseJSONCat();
   }
 })
