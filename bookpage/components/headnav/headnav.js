6a2a040cd0e4ae9a3784bf6c20ca98acfd064260
NicholasKrause <NicholasKrause@users.noreply.sudo>
2017-09-25 18:08:10 -0400

    content

--- bookpage/components/headnav/headnav.js
+++ bookpage/components/headnav/headnav.js
@@ -10,8 +10,7 @@ Lakeside prod enter 143210 (lsc) then execute this snippet
 
 sudo curl -o /Users/neodigm/2016/is-ecom-labs/ta.json "https://www.lakeside.com/common/includes/inc_search_type_ahead.jsp?q=1418930&limit=12&timestamp=1504554461655&environment=typeAhead_queries&sort=alpha&searchFrom=&Ntt=1418930*&_=1504554389332"
 
-TODO | Convert CR delimit string to Array within Array | Brand logic | Build Query string | Click Go
-TODO | Unit test in IE
+TODO | Brand logic | Build Query string
 
 */
 
@@ -20,7 +19,6 @@ var vTA = new Vue({
 	data: {
 		ta_data: "",
 		ta_response: "",
-	ta_responseTMP: [["123", "ABC"],["345","EFG"]],
 		is_hidden: true
 	},
 	watch: {
@@ -41,9 +39,12 @@ var vTA = new Vue({
 			//axios.get( 'https://www.lakeside.com/common/includes/inc_search_type_ahead.jsp?q=1418930&limit=12&timestamp=1504554461655&environment=typeAhead_queries&sort=alpha&searchFrom=&Ntt=1418930*&_=1504554389332' )
 			axios.get( "ta.html" )
 			.then(function ( response ) {
-				var aPins = response.data.split( "\n" ); // may need to pop last value
-		console.log( aPins );
-				vTA.ta_response = response;
+				var aTmp = [], aPins = response.data.split( "\n" ); // may need to pop last value
+				for (var i=0; i<(aPins.length - 1); ++i) {
+					var nPos = aPins[i].indexOf(" ");
+					aTmp.push( [aPins[i].substr(0,nPos), aPins[i].substr(nPos)] );
+				}
+				vTA.ta_response = aTmp;
 			})
 			.catch(function ( error ) {
 				vTA.ta_response = "";
@@ -51,6 +52,10 @@ var vTA = new Vue({
 		},
 		pinClick: function( e ){
 			this.ta_data = e.currentTarget.firstChild.innerHTML;
+			var eF = document.getElementById("basicSearch");
+			if( eF ){
+				eF.submit();
+			}
 		}
 	},
 	created: function() {
