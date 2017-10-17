eef8372ddb18e267ea649c85fe3d358f50b6874a
neodigm <neo5ive@gmail.com>
2017-10-17 14:53:43 -0500

    content

---
+++ bookpage/components/flyoutcart/flyoutcart.js
@@ -0,0 +1,19 @@
+"use strict";
+console.log("component flyoutcart.js");
+
+var ltdc_flyoutcart = (function( doc, eID ) {
+  var nWidthAdj = doc.documentElement.clientWidth;
+	if ( nWidthAdj >= 980 ) { // Position horz if large
+    doc.getElementById( eID ).style.right = ( ((nWidthAdj - 980) / 2) + "px" );
+	}
+  //  wire close ancher
+  return {
+    open: function(){
+      doc.getElementById( eID ).classList.remove("hidden");
+      setTimeout( this.close, 2800);
+    },
+    close: function(){
+      doc.getElementById( eID ).classList.add("hidden");
+    }
+  }
+})( document, "js-flyoutcart__id" );
--- bookpage/components/productcard/productcard.js
+++ bookpage/components/productcard/productcard.js
@@ -8,7 +8,7 @@ console.log("component productcard.js");
       ltdc_productcard.eMenu = document.getElementById( "js-drawerright__menu" );
       ltdc_productcard.eScrim = document.getElementById( "js-drawerrignt__scrim" );
       ltdc_productcard.eScrim.addEventListener("click", ltdc_productcard.close, false);
-      ltdc_productcard.eQV = document.getElementsByClassName( "js-productcard__a--qv" );
+      ltdc_productcard.eQV = document.getElementsByClassName( "QQQQjs-productcard__a--qv" );
       for (var i = 0, ln = ltdc_productcard.eQV.length; i < ln; i++) {
           ltdc_productcard.eQV[i].addEventListener("click", ltdc_productcard.open, false);
       }
@@ -19,11 +19,17 @@ console.log("component productcard.js");
     },
     "close": function(){
       ltdc_productcard.eMenu.classList.remove("isOpenR__menu");
-      ltdc_productcard.eScrim.classList.remove("isOpenR__scrim");  
+      ltdc_productcard.eScrim.classList.remove("isOpenR__scrim");
     }
   };
   ltdc_productcard.init();
 })();
+
+function doaction(var1, var2, var3, var4, var5, var6, showSoldOut) {
+console.log("doaction | ");
+console.log( this );
+  return true;
+}
 
 /*
 var c_productcard = new Vue({
