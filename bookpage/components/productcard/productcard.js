3ed0e1be5a23c6003110b563ef3334aaf4c5ab4e
neodigm <neo5ive@gmail.com>
2017-11-01 09:32:01 -0500

    content

--- bookpage/components/productcard/productcard.js
+++ bookpage/components/productcard/productcard.js
@@ -3,12 +3,14 @@ console.log("component productcard.js");
 
 (function(){
   var ltdc_productcard = {
-    eQV: null, eMenu: null, eScrim: null,
+    eQV: null, eMenu: null, eScrim: null, eX: null,
     "init": function(){
       ltdc_productcard.eMenu = document.getElementById( "js-drawerright__menu" );
       ltdc_productcard.eScrim = document.getElementById( "js-drawerrignt__scrim" );
       ltdc_productcard.eScrim.addEventListener("click", ltdc_productcard.close, false);
-      ltdc_productcard.eQV = document.getElementsByClassName( "QQQQjs-productcard__a--qv" );
+      ltdc_productcard.eX = document.getElementById( "js-drawerright__x" );
+      ltdc_productcard.eX.addEventListener("click", ltdc_productcard.close, false);
+      ltdc_productcard.eQV = document.getElementsByClassName( "js-productcard__a--qv" );
       for (var i = 0, ln = ltdc_productcard.eQV.length; i < ln; i++) {
           ltdc_productcard.eQV[i].addEventListener("click", ltdc_productcard.open, false);
       }
@@ -24,12 +26,6 @@ console.log("component productcard.js");
   };
   ltdc_productcard.init();
 })();
-
-function doaction(var1, var2, var3, var4, var5, var6, showSoldOut) {
-console.log("doaction | ");
-console.log( this );
-  return true;
-}
 
 /*
 var c_productcard = new Vue({
