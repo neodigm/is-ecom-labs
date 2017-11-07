53c464741286034e6d1b61c7c60a84d2a71a337e
neodigm <neo5ive@gmail.com>
2017-11-07 15:16:03 -0600

    content

--- bookpage/components/flyoutcart/flyoutcart.js
+++ bookpage/components/flyoutcart/flyoutcart.js
@@ -4,7 +4,7 @@ console.log("component flyoutcart.js");
 var ltdc_flyoutcart = (function( doc, eID ) {
   var nWidthAdj = doc.documentElement.clientWidth;
 	if ( nWidthAdj >= 980 ) { // Position horz if large
-    doc.getElementById( eID ).style.right = ( ((nWidthAdj - 980) / 2) + 210 + "px" );
+    doc.getElementById( eID ).style.right = ( ((nWidthAdj - 980) / 2) + 212 + "px" );
 	}
   //  wire close ancher
   return {
