f7fdc01db045bb3253768c233be3071fe5071177
neodigm <neo5ive@gmail.com>
2017-11-07 16:51:56 -0600

    content

--- bookpage/components/flyoutcart/flyoutcart.js
+++ bookpage/components/flyoutcart/flyoutcart.js
@@ -4,12 +4,13 @@ console.log("component flyoutcart.js");
 var ltdc_flyoutcart = (function( doc, eID ) {
   var nWidthAdj = doc.documentElement.clientWidth;
 	if ( nWidthAdj >= 980 ) { // Position horz if large
-    //doc.getElementById( eID ).style.right = ( ((nWidthAdj - 980) / 2) + 212 + "px" );
-    doc.getElementById( eID ).style.right = "352px";
+    doc.getElementById( eID ).style.right = ( ((nWidthAdj - 980) / 2) + 212 + "px" );
 	}
   //  wire close ancher
   return {
     open: function(){
+      doc.getElementById( eID ).style.right = "352px"; // assumes that QV is open
+      doc.getElementById( eID ).style.top = document.documentElement.scrollTop + "px"; // assumes that QV is open
       doc.getElementById( eID ).classList.remove("hidden");
       setTimeout( this.close, 2800);
     },
