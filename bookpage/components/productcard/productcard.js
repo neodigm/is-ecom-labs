2388829d064564d71e132de4081e4271b9938cc3
neodigm <neo5ive@gmail.com>
2017-11-13 17:05:16 -0600

    content

--- bookpage/components/productcard/productcard.js
+++ bookpage/components/productcard/productcard.js
@@ -6,7 +6,7 @@ console.log("component productcard.js");
     eQV: null, eMenu: null, eScrim: null, eX: null, eBody: null, ePersInstr: null, bPers: false,
     "init": function(){
       ltdc_productcard.eBody = document.getElementsByTagName("BODY")[0];
-      ltdc_productcard.ePersInstr = document.getElementById("js-qv-persinstr__contianer--id");
+      ltdc_productcard.ePersInstr = document.getElementById("js-qv-persinstr__container--id");
       document.getElementById("js-qv-persinstr__handle--id").addEventListener("click", ltdc_productcard.toggle_pers, false);
       ltdc_productcard.eMenu = document.getElementById( "js-drawerright__menu" );
       ltdc_productcard.eScrim = document.getElementById( "js-drawerrignt__scrim" );
@@ -21,17 +21,17 @@ console.log("component productcard.js");
     "open": function(){
       ltdc_productcard.eBody.classList.add("qv--open");
       //ltdc_productcard.eMenu.classList.add("isOpenR__menu");
-    ltdc_productcard.eMenu.style.width = "352px";
+    ltdc_productcard.eMenu.style.width = "544px";
     TweenMax.to(document.getElementById("js-drawerright__menu"), .8, {right: 0, ease:Power4.easeInOut});
     ltdc_productcard.eScrim.classList.add("isOpenR__scrim");
-    TweenMax.to(ltdc_productcard.ePersInstr, 1, {right: -156, ease:Power4.easeInOut, delay:0.6});
+    TweenMax.to(ltdc_productcard.ePersInstr, 1, {right: 38, ease:Power4.easeInOut, delay:0.6});
     },
     "close": function(){
       //ltdc_productcard.eMenu.classList.remove("isOpenR__menu");
       ltdc_productcard.bPers = false;
       TweenMax.to(ltdc_productcard.ePersInstr, .3, {right: -560, ease:Power4.easeInOut,
       onComplete:function(){
-        TweenMax.to(document.getElementById("js-drawerright__menu"), .6, {right: -380, ease:Power4.easeInOut,
+        TweenMax.to(document.getElementById("js-drawerright__menu"), .6, {right: -580, ease:Power4.easeInOut,
           onComplete:function(){
           ltdc_productcard.eBody.classList.remove("qv--open");
           ltdc_productcard.eScrim.classList.remove("isOpenR__scrim");
@@ -40,9 +40,9 @@ console.log("component productcard.js");
     },
     "toggle_pers": function(){
       if( ltdc_productcard.bPers ){
-        TweenMax.to(ltdc_productcard.ePersInstr, 1, {right: -156, ease:Power4.easeInOut});
+        TweenMax.to(ltdc_productcard.ePersInstr, 1, {right: 38, ease:Power4.easeInOut});
       }else{
-        TweenMax.to(ltdc_productcard.ePersInstr, 1, {right:  324, ease:Power4.easeInOut});
+        TweenMax.to(ltdc_productcard.ePersInstr, 1, {right:  464, ease:Power4.easeInOut});
       }
       ltdc_productcard.bPers = !ltdc_productcard.bPers;
     }
