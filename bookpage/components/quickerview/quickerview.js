184f2987063f9e6b0b456555edfd253bfcc6d40d
neodigm <neo5ive@gmail.com>
2017-12-06 15:59:24 -0600

    content

--- bookpage/components/quickerview/quickerview.js
+++ bookpage/components/quickerview/quickerview.js
@@ -1,2 +1,11 @@
 "use strict";
 console.log("component quickerview.js");
+
+
+
+	document.getElementById("js-qv__button-a2c--id").addEventListener("click", function(){
+    var eQty = document.getElementById( "js-qty__input--id" );
+    if( eQty && (eQty.value > 0) ){
+		    ltdc_snackbar.q(eQty.value + " item(s) added to your shopping cart.");  
+    }
+	});
