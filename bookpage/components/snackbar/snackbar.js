7d500d33123d1c0a25ac08936879f4d9f8fa8e60
neodigm <neo5ive@gmail.com>
2017-12-07 11:33:17 -0600

    content

--- bookpage/components/snackbar/snackbar.js
+++ bookpage/components/snackbar/snackbar.js
@@ -8,7 +8,7 @@ var ltdc_snackbar = (function( doc, eID ) {
     _aQ.shift(); // fifo
     _eSb.classList.remove("snackbar__cont--show");
     _eSb.classList.add("snackbar__cont--hide");
-    if( _aQ.length != 0 ){ setTimeout( _fOpen, 1200 ); } // If more messages open again
+    if( _aQ.length != 0 ){ setTimeout( _fOpen, 1200 ); } // If more messages then open again
   };
   var _fOpen = function(){
     _eSbText.innerHTML = _aQ[0].replace("|","<br>");
@@ -19,7 +19,7 @@ var ltdc_snackbar = (function( doc, eID ) {
   return {
     q: function( sMsg ){
       _aQ.push( sMsg ); // fifo
-      if( _aQ.length == 1 ){ _fOpen(); } // If first message open
+      if( _aQ.length == 1 ){ _fOpen(); } // If first message then init open
     }
   }
 })( document, "js-snackbar__id" );
