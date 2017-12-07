6f348149b5f166556141ab13ad145cd3a6993ea9
neodigm <neo5ive@gmail.com>
2017-12-07 17:38:44 -0600

    content

--- bookpage/components/quickerview/quickerview.js
+++ bookpage/components/quickerview/quickerview.js
@@ -10,28 +10,29 @@ var ltdc_quickerview_options = {
 
 var ltdc_quickerview = (function( doc, Opt ) {
   var nQty = 1;
-doc.getElementById( Opt.qty_input_id ).addEventListener("change", function(){
-    nQty = doc.getElementById( Opt.qty_input_id ).value;
-});
-doc.getElementById( Opt.button_minus_id ).addEventListener("click", function(){
-    if( nQty > 0){
-      doc.getElementById( Opt.qty_input_id ).value = (--nQty);
+  //  Below is non-reactive proto code that needs to be replaced
+  doc.getElementById( Opt.qty_input_id ).addEventListener("change", function(){
+      nQty = doc.getElementById( Opt.qty_input_id ).value;
+  });
+  doc.getElementById( Opt.button_minus_id ).addEventListener("click", function(){
+      if( nQty > 0){
+        doc.getElementById( Opt.qty_input_id ).value = (--nQty);
+      }
+  });
+  doc.getElementById( Opt.button_plus_id ).addEventListener("click", function(){
+      doc.getElementById( Opt.qty_input_id ).value = (++nQty);
+  });
+
+  doc.getElementById( Opt.button_a2c_id ).addEventListener("click", function(){
+    var eQty = doc.getElementById( "js-qty__input--id" );
+    var eSel = doc.getElementById( "js-person-select--id" );
+
+    if( eQty && (eQty.value > 0) ){
+  	    ltdc_snackbar.q( eSel.value + "|" + nQty + " item(s) added to your shopping cart." );
+        nQty = 1;
+        doc.getElementById( Opt.qty_input_id ).value = 1;
     }
-});
-doc.getElementById( Opt.button_plus_id ).addEventListener("click", function(){
-    doc.getElementById( Opt.qty_input_id ).value = (++nQty);
-});
-
-doc.getElementById( Opt.button_a2c_id ).addEventListener("click", function(){
-  var eQty = doc.getElementById( "js-qty__input--id" );
-  var eSel = doc.getElementById( "js-person-select--id" );
-
-  if( eQty && (eQty.value > 0) ){
-	    ltdc_snackbar.q( eSel.value + "|" + nQty + " item(s) added to your shopping cart." );
-      nQty = 1;
-      doc.getElementById( Opt.qty_input_id ).value = 1;
-  }
-});
+  });
   return {
     addToCart: function(){
     }
